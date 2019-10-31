7335c2ed630bfb70f81904adbc1030d2329cccb3
Mystic Quest <l1skou@gmail.com>
2019-10-31 15:21:36 +0200

    Initial upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  //res.sendFile(__dirname + "/index  .html");
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = new Date().toLocaleDateString("en-US", options);
+
+  res.render('list', {
+    kindOfDay: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port: 3000");
+});
