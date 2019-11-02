d7205f3b36cedeea51a39199d1258427d4184039
Mystic Quest <l1skou@gmail.com>
2019-11-02 14:44:35 +0200

    added date module, improved ejs, css

--- app.js
+++ app.js
@@ -1,43 +1,56 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = [];
-
 app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(express.static("public"));
+
+const items = [];
+const workItems = [];
 
 app.get("/", function(req, res) {
-  //res.sendFile(__dirname + "/index  .html");
 
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
+  const day = date.getDate();
 
-  var day = new Date().toLocaleDateString("en-US", options);
-
-  res.render('list', {
-    kindOfDay: day,
+  res.render("list", {
+    listTitle: day,
     newListItems: items
   });
 
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
-  if (item !== "") {
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
     items.push(item);
+    res.redirect("/");
   }
-  res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port: 3000");
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
