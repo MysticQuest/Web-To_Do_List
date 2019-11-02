1fe227225c8909af766db6a06d84b12d00bd25fa
Mystic Quest <l1skou@gmail.com>
2019-11-02 19:44:41 +0200

    added dynamic lists by url

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -38,11 +39,21 @@ const itemsSchema = {
 
 const Item = mongoose.model("item", itemsSchema);
 
-//routes
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+
+//-----------------------ROUTES -------------------------------
+
+
+const day = date.getDate();
 
 app.get("/", function(req, res) {
-
-  const day = date.getDate();
 
   Item.find({}, function(err, results) {
     res.render("list", {
@@ -52,18 +63,59 @@ app.get("/", function(req, res) {
   });
 });
 
+app.get("/:customListName", function(req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: []
+        });
+
+        list.save();
+        res.redirect("/" + customListName);
+
+      } else {
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
+      }
+    }
+  });
+});
 
 
 app.post("/", function(req, res) {
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
+
   if (itemName.length > 2) {
     const item = new Item({
       name: itemName,
     });
-    item.save();
-    console.log("Item saved successfully!");
-    res.redirect("/");
+
+    if (listName === day) {
+      item.save();
+      res.redirect("/");
+      console.log("Item added to main list!")
+    } else {
+      List.findOne({
+        name: listName
+      }, function(err, foundList) {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+        console.log("Item added to custom list!");
+      })
+
+    }
   } else {
     console.log("Please enter something valid");
   }
@@ -72,22 +124,35 @@ app.post("/", function(req, res) {
 app.post("/delete",
   function(req, res) {
     const checkedItemID = req.body.checkbox;
-    Item.findByIdAndRemove(checkedItemID, function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.redirect("/");
-        console.log("Item deleted succcessfully");
-      }
-    });
+    const listName = req.body.listName;
+
+    if (listName === day) {
+      Item.findByIdAndRemove(checkedItemID, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect("/");
+          console.log("Item deleted succcessfully");
+        }
+      });
+    } else {
+      List.findOneAndUpdate({
+        name: listName
+      }, {
+        $pull: {
+          items: {
+            _id: checkedItemID
+          }
+        }
+      }, function(err, foundList) {
+        if (!err) {
+          res.redirect("/" + listName);
+          console.log("Item deleted succcessfully from custom list");
+        }
+      });
+    }
   });
 
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
-  });
-});
 
 app.get("/about", function(req, res) {
   res.render("about");
